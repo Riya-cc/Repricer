772997afd1ef70cfff6a101fc74253f957c351d8
Riya-CC <riya.bulia@curaechoice.com>
2023-06-16 18:49:55 +0530

    Create Tab.jsx

---
+++ src/components/Tab.jsx
@@ -0,0 +1,62 @@
+function Tab() {
+return(
+<div className="self-stretch flex flex-row items-center justify-between">
+  <div className="w-[460px] h-[75px] flex flex-col px-4 box-border items-center justify-center relative">
+    <div className="flex flex-row items-center justify-center gap-[8px] z-[0]">
+      {/* <img className="relative w-[25px] h-[25px]" alt="" src="/edit-icon.svg" /> */}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+        <path
+          d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+        <path
+          d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+      </svg>
+      <div
+        className="relative text-lg tracking-[1px] font-semibold font-big-description text-darkslategray text-center flex items-center justify-center w-[247.05px] shrink-0 uppercase">
+        Manual Entry
+      </div>
+    </div>
+    {/* <img className="absolute my-0 mx-[!important] top-[72px] left-[51.5px] w-[390px] h-1.5 z-[1]" alt=""
+      src="/line-1.svg" /> */}
+
+  </div>
+  {/* <div className="rounded w-[459px] h-[75px] flex flex-row px-4 box-border items-center justify-center">
+    <div className="flex flex-row items-center justify-center gap-[8px] z-[0]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+        <path fill-rule="evenodd"
+          d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
+          clip-rule="evenodd" />
+        <path
+          d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+      </svg>
+
+      <div
+        className="relative top-[9.5px] left-[48px] text-lg tracking-[1px] font-semibold font-big-description text-darkslategray text-center flex items-center justify-center w-[224.27px]">
+        FILE UPLOAD
+      </div>
+    </div>
+  </div> */}
+
+<div className="w-[460px] h-[75px] flex flex-col px-4 box-border items-center justify-center relative">
+    <div className="flex flex-row items-center justify-center gap-[8px] z-[0]">
+      {/* <img className="relative w-[25px] h-[25px]" alt="" src="/edit-icon.svg" /> */}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+        <path fill-rule="evenodd"
+          d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
+          clip-rule="evenodd" />
+        <path
+          d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+      </svg>
+      <div
+        className="relative text-lg tracking-[1px] font-semibold font-big-description text-darkslategray text-center flex items-center justify-center w-[247.05px] shrink-0">
+        File Upload
+      </div>
+    </div>
+    {/* <img className="absolute my-0 mx-[!important] top-[72px] left-[51.5px] w-[390px] h-1.5 z-[1]" alt=""
+      src="/line-1.svg" /> */}
+
+  </div>
+</div>
+)
+}
+
+export default Tab;
