8b5029d7a660099b06479813cc12119b37897cba
Riya-CC <riya.bulia@curaechoice.com>
2023-06-16 22:52:12 +0530

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import Tab from "./Tab";
+import InpatientForm from "./InpatientForm";
+
+function Main() {
+  return (
+    <>
+      <main className="bg-aliceblue h-fit flex flex-col items-center justify-start text-center text-2xl text-darkslategray font-big-description rounded-tl-11xl rounded-bl-11xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]">
+        <div className=" flex flex-col pt-5 px-10 box-border items-center justify-start gap-[37px]">
+          <Tab />
+          <InpatientForm />
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Main;
