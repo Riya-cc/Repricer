8f46b21743c369d2be12cc0e61a74802a119712e
Riya-CC <riya.bulia@curaechoice.com>
2023-06-16 22:52:03 +0530

    Update Tab, Sidebar and App style

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
-// import Footer from './components/Footer'; 
 
 function App() {
-  return (
-    <>
-     <div className="font-roboto bg-white">
-      <Header />
-      <div className="flex flex-row mx-16 w-full">
-        <Sidebar />
-        <Main />
-      </div>
-      {/* <Footer /> */}
-      </div>
-    </>
-  );
+return (
+<>
+  <div className="font-roboto bg-white">
+    <Header />
+    <div className="flex flex-row px-40 gap-12">
+      <Sidebar />
+      <Main />
+    </div>
+  </div>
+</>
+);
 }
 
 export default App;
