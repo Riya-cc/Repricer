f786639eb6887db8edb830444752b3e3e9531eb8
Riya-CC <135866304+Riya-cc@users.noreply.github.com>
2023-06-20 00:32:49 +0530

    Add bg image and Tab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
+import topBg from './assets/top-bg.png';
+import Header from './pages/Header';
+import InteractiveTabs from './components/form-inputs/InteractiveTabs';
 
 function App() {
 return (
 <>
-  <div className="font-roboto bg-white">
+    <div className="font-roboto bg-aliceblue">
+      <img className='absolute right-[-2px] top-0 w-96' src={topBg} alt="top background"/>
     <Header />
-    <div className="flex flex-row px-40 gap-12">
-      <Sidebar />
-      <Main />
+      <div className="flex flex-row px-40 gap-12">
+      <InteractiveTabs />
     </div>
   </div>
 </>
