bb8643c0ac7528b5cb9419a0916805696d8cc447
Riya-CC <135866304+Riya-cc@users.noreply.github.com>
2023-06-19 17:42:59 +0530

    Update Tab.jsx

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -3,7 +3,7 @@ return(
 <div className="self-stretch flex flex-row items-center">
   <div className="w-1/2 h-[75px] flex flex-col px-4 box-border items-center justify-center relative">
     <div className="flex flex-row items-center justify-center gap-[8px] ">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
         <path
           d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
         <path
@@ -18,10 +18,10 @@ return(
 
   <div className="w-1/2 h-[75px] flex flex-col px-4 box-border items-center justify-center relative">
     <div className="flex flex-row items-center justify-center gap-[8px] z-[0]">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-        <path fill-rule="evenodd"
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+        <path fillRule="evenodd"
           d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
-          clip-rule="evenodd" />
+          clipRule="evenodd" />
         <path
           d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
       </svg>
