a91abceb7f399b8b22a62a3fb27b52c83e2f25f2
Riya-CC <riya.bulia@curaechoice.com>
2023-06-20 21:43:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import Header from './pages/Header';
 import InteractiveTabs from './pages/InteractiveTabs';
 
 function App() {
-return (
-<>
-    <div className="font-roboto bg-aliceblue">
-      <img className='absolute right-[-2px] top-0 w-96' src={topBg} alt="top background"/>
-    <Header />
-      <div className="flex flex-col md:px-[320px] mt-[70px]">
-        <InteractiveTabs />
-       
-    </div>
-  </div>
-</>
-);
+  return (
+    <>
+      <div className="font-roboto bg-aliceblue">
+        <img className='absolute right-[-2px] top-0 w-96' src={topBg} alt="top background" />
+        <Header />
+        <div className="flex flex-col md:px-[320px] mt-[70px]">
+          <InteractiveTabs />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
