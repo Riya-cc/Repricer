97b8818f3ff6ac6f8beb44ba5f75e59a33422477
Riya-CC <riya.bulia@curaechoice.com>
2023-06-20 21:43:48 +0530

    Add label to radio group

--- src/components/form-inputs/CustomRadio.jsx
+++ src/components/form-inputs/CustomRadio.jsx
@@ -1,23 +1,28 @@
-function CustomRadio({ radioItems }) {
+function CustomRadio({ radioItems, label }) {
   return (
-    <div className="flex">
-      {radioItems.map((item) => (
-        <div className="flex items-center mr-4" key={item.id}>
-          <input
-            id={item.id}
-            type="radio"
-            value=""
-            name="inline-radio-group"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-black focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            htmlFor={item.id}
-            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            {item.label}
-          </label>
-        </div>
-      ))}
+    <div className="flex flex-col items-start gap-1 grow">
+      <label htmlFor={label} className="px-1 font-normal text-base leading-4 text-darkslategray">{label}</label>
+      <div className="flex">
+        {radioItems.map((item) => (
+          <>
+            <div className="flex items-center mr-4" key={item.id}>
+              <input
+                id={item.id}
+                type="radio"
+                value=""
+                name="inline-radio-group"
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-black focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor={item.id}
+                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                {item.label}
+              </label>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
