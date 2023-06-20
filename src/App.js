ec8aeed7952457bc7f5a2ad2023085e4e70eb15a
Riya-CC <riya.bulia@curaechoice.com>
2023-06-20 17:58:04 +0530

    Add form and tabs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import topBg from './assets/top-bg.png';
 import Header from './pages/Header';
-import InteractiveTabs from './components/form-inputs/InteractiveTabs';
+import InteractiveTabs from './pages/InteractiveTabs';
 
 function App() {
 return (
@@ -9,8 +9,9 @@ return (
     <div className="font-roboto bg-aliceblue">
       <img className='absolute right-[-2px] top-0 w-96' src={topBg} alt="top background"/>
     <Header />
-      <div className="flex flex-row px-40 gap-12">
-      <InteractiveTabs />
+      <div className="flex flex-col md:px-[320px] mt-[70px]">
+        <InteractiveTabs />
+       
     </div>
   </div>
 </>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import Tab from "./Tab";
+import Tab from "./form-inputs/Tab";
 import InpatientForm from "./InpatientForm";
 
 function Main() {
@@ -6,8 +6,7 @@ function Main() {
     <>
       <main className="bg-aliceblue h-fit flex flex-col items-center justify-start text-center text-2xl text-darkslategray font-big-description rounded-tl-11xl rounded-bl-11xl shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]">
         <div className=" flex flex-col pt-5 px-10 box-border items-center justify-start gap-[37px]">
-          <Tab />
-          <InpatientForm />
+          
         </div>
       </main>
     </>
