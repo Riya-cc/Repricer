38d2d39fd2fd56e0350229386f21bd5d5e2b8387
Riya-CC <riya.bulia@curaechoice.com>
2023-06-20 17:56:04 +0530

    Create CustomInput.jsx

---
+++ src/components/form-inputs/CustomInput.jsx
@@ -0,0 +1,13 @@
+function DynamicInput({label}) {
+return (
+<>
+  <div className="flex flex-col items-start gap-1">
+      <label htmlFor={label} className="px-1 font-normal text-base leading-4 text-darkslategray">{label}</label>
+      <input type="text" id="first_name" className="border-2 border-lightsteelblue text-darkslategray text-sm rounded-lg focus:ring-slate-blue-l2 focus:border-slate-blue-l2 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-blue-l2 dark:focus:border-slate-blue-l2"
+      placeholder="" required />
+  </div>
+</>
+);
+}
+
+export default DynamicInput;
